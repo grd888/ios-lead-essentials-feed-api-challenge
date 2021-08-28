475f117133bce439fa84f6ddf5360671e1c62260
Greg Delgado <grd888@gmail.com>
2021-08-28 18:26:24 +0800

    change RemoteFeedLoader.Error.connectivity to simply Error.connectivity since Error is declared under RemoteFeedLoader

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -27,7 +27,7 @@ public final class RemoteFeedLoader: FeedLoader {
 			case let .success((data, response)):
 				completion(FeedImagesMapper.map(data, from: response))
 			case .failure:
-				completion(.failure(RemoteFeedLoader.Error.connectivity))
+				completion(.failure(Error.connectivity))
 			}
 		}
 	}
